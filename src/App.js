e2800988e4830de83d183979234fcaf2785a4a3b
Anand Raj <anandrajj10@gmail.com>
2022-12-13 22:28:43 +0530

    Final

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import React, { useState } from "react";
 import Expenses from "./components/Expense/Expenses";
 import Header from "./components/Header";
 import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
-  const expenses = [
+  const DUMMY_EXPENSES = [
     {
       id: "e1",
       title: "Toilet Paper",
@@ -24,10 +25,12 @@ const App = () => {
       date: new Date(2021, 5, 12),
     },
   ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
   const addExpenseHandler = (expense) => {
-    console.log(expense);
-    expenses.push(expense);
-    console.log(expenses);
+    setExpenses((prevState) => {
+      return [expense, ...expenses];
+    });
   };
   return (
     <div>
