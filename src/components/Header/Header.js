3a95a1e3abe866d3cafde2b0ee40e0f59b4e83bc
Anand Raj <anandrajj10@gmail.com>
2022-12-29 15:43:21 +0530

    header added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header-section">
+      <h1>Expense Calculator Application</h1>
+    </div>
+  );
+};
+
+export default Header;
